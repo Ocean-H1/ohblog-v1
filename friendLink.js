4be4b1a9824c5516779af82b77546ca092efea19
OceanH <1404939185@qq.com>
2024-03-06 16:38:07 +0800

    feat: 添加友链

--- friendLink.js
+++ friendLink.js
@@ -3,13 +3,22 @@
  * @Author: OceanH
  * @Date: 2023-08-07 23:58:45
  */
-
 const friendLink = [
   {
     title: 'Cai.',
     desc: 'Enjoy when you can, and endure when you must.',
     link: 'https://www.awesomeboy.cn/'
   },
+  {
+    title: 'Aurora',
+    desc: 'Aurora的个人博客',
+    link: 'http://wei-z.top/'
+  },
+  {
+    title: '小关同学',
+    desc: '小关同学の个人博客',
+    link: 'http://www.xiaoguantongxue.com/#/index'
+  }
 ];
 
 module.exports = {
